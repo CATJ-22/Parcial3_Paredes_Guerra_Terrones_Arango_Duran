afe842a07c4e743e2ff6f84d03298d47556d725d
Kiriam02 <59971744+Kiriam02@users.noreply.github.com>
2020-06-23 00:26:59 -0500

    Creación de la base de datos, de las tablas Recipes y Users

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/MainActivity.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.parcial3_paredes_guerra_terrones_arango_duran;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int DURACION_SPLASH = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.ImpresionDatos();
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
@@ -25,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
             ;
         }, DURACION_SPLASH);
 
+    }
+
+
+    private void ImpresionDatos() {
+        try {
+            BD_Recetas smDB = new BD_Recetas(this, "Users", null, 1);
+            SQLiteDatabase db = smDB.getWritableDatabase();
+        } catch (Exception e) {
+
+        }
     }
 }
