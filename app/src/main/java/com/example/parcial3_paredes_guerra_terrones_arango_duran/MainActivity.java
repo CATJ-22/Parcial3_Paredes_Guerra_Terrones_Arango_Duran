c51a9658cc09a1847c4717564c76e3fa5c54992d
Catj99 <cristian.terrones@hotmail.com>
2020-06-22 21:02:51 -0500

    restaured

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/MainActivity.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//hola
 
-    //gato
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
