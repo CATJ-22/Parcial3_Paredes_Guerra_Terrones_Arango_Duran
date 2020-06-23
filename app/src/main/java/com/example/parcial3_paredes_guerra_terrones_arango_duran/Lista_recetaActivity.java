8886a99c70801e208f4c1fbd499c3f5d0485b635
kendall2 <kguerra839@gmail.com>
2020-06-22 21:32:02 -0500

    Activity-lista-receta y xml activity-lista-receta 1.1

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Lista_recetaActivity.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Lista_recetaActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Lista_receta extends AppCompatActivity {
+public class Lista_recetaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_lista_receta);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_receta);
     }
 }
