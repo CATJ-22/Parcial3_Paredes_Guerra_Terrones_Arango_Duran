72b96faac1523b98785717403121574345dffa65
Catj99 <cristian.terrones@hotmail.com>
2020-06-22 21:56:27 -0500

    Integracion del codigo splash

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/MainActivity.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.parcial3_paredes_guerra_terrones_arango_duran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int DURACION_SPLASH = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            ;
+        }, DURACION_SPLASH);
+
     }
 }
