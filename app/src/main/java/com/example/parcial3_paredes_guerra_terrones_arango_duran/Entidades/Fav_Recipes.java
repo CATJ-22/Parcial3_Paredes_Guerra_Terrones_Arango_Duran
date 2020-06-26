eee290943f722c5505c8be6f046077752c4441af
Kiriam02 <59971744+Kiriam02@users.noreply.github.com>
2020-06-26 02:03:24 -0500

    Cambios en la base de datos  Tabla Fav_Recipes, se agrego la columna (like_recipe) para recetas que le gusto al usuario y (dislike_recipe) para recetas que no le gusto al usuario

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Entidades/Fav_Recipes.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Entidades/Fav_Recipes.java
@@ -3,11 +3,15 @@ package com.example.parcial3_paredes_guerra_terrones_arango_duran.Entidades;
 public class Fav_Recipes {
     private int id_user;
     private int id_recipes;
+    private String like_recipe;
+    private String dislike_recipe;
 
-    public Fav_Recipes( int idu, int idr){
+    public Fav_Recipes( int idu, int idr,String lr, String dr){
 
         id_user= idu;
         id_recipes=idr;
+        like_recipe=lr;
+        dislike_recipe=dr;
 
     }
 
@@ -26,6 +30,14 @@ public class Fav_Recipes {
 
     public void setId_recipes(int id_recipes) {
         this.id_recipes = id_recipes;
+    }
+
+    public String getLike_recipe() {
+        return like_recipe;
+    }
+
+    public String getDislike_recipe() {
+        return dislike_recipe;
     }
 }
 
