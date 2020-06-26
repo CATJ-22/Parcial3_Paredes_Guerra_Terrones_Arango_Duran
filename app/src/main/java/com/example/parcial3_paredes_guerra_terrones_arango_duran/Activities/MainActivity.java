8a845ef95e1801beba420a16dcca38113d705fe8
Catj99 <cristian.terrones@hotmail.com>
2020-06-25 21:41:41 -0500

    testeo

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Activities/MainActivity.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Activities/MainActivity.java
@@ -47,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
             db.execSQL("INSERT INTO users (name, email, password, type)" + "VALUES ('elfo','elfo@example.com','123','Normal')");
             db.execSQL("INSERT INTO recipes (name, description, ingredients, restaurant, comments)" + "VALUES ('hamborguesa','cuadrada','pan carne queso y salsas','mcquetedandonals', 'jaja muy good')");
             db.execSQL("INSERT INTO fav_recipe (id_user, id_recipes)" + "VALUES (0,0)");
+            db.execSQL("INSERT INTO recipes (name, description, ingredients, restaurant, comments)" + "VALUES ('panqueso','redondo','pan queso y mantequilla','zamosandwich', 'jaja verymucho good')");
+            db.execSQL("INSERT INTO fav_recipe (id_user, id_recipes)" + "VALUES (1,1)");
             /*KIRIAM
             if(db != null) {
                 db.execSQL("INSERT INTO users (name, email, password, type)" + "VALUES ('zamora','zamora@example.com','123','Administrador')");
