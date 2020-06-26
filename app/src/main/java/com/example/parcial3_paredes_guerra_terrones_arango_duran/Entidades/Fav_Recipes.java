d4fb278cd218a9015bda7cb027e95183574bc240
Kiriam02 <59971744+Kiriam02@users.noreply.github.com>
2020-06-26 04:21:18 -0500

    Tabla Fav_Recipes, se ELIMINI LA COLUMNA ID_USER y se agrego name

--- app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Entidades/Fav_Recipes.java
+++ app/src/main/java/com/example/parcial3_paredes_guerra_terrones_arango_duran/Entidades/Fav_Recipes.java
@@ -1,35 +1,25 @@
 package com.example.parcial3_paredes_guerra_terrones_arango_duran.Entidades;
 
 public class Fav_Recipes {
-    private int id_user;
+    private String name;
     private int id_recipes;
     private String like_recipe;
     private String dislike_recipe;
 
-    public Fav_Recipes( int idu, int idr,String lr, String dr){
-
-        id_user= idu;
+    public Fav_Recipes(String n , int idr,String lr, String dr){
+        name=n;
         id_recipes=idr;
         like_recipe=lr;
         dislike_recipe=dr;
 
     }
 
-
-    public int getId_user() {
-        return id_user;
-    }
-
-    public void setId_user(int id_user) {
-        this.id_user = id_user;
+    public String getName() {
+        return name;
     }
 
     public int getId_recipes() {
         return id_recipes;
-    }
-
-    public void setId_recipes(int id_recipes) {
-        this.id_recipes = id_recipes;
     }
 
     public String getLike_recipe() {
